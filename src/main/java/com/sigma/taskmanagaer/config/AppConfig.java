5e16bf869fe68cb7082d263fe70df1f2cf087bd7
abdelrhman <abdelrhman225328@bue.edu.eg>
2023-09-19 14:38:16 +0300

    A.shenawy

--- src/main/java/com/sigma/taskmanagaer/config/AppConfig.java
+++ src/main/java/com/sigma/taskmanagaer/config/AppConfig.java
@@ -6,7 +6,15 @@ import com.sigma.taskmanagaer.entity.Task;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
 
 @Configuration
 public class AppConfig  implements RepositoryRestConfigurer{
@@ -15,5 +23,23 @@ public class AppConfig  implements RepositoryRestConfigurer{
         config.exposeIdsFor(Staff.class);
         config.exposeIdsFor(Project.class);
         config.exposeIdsFor(Task.class);
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+
+        // Allow requests from any origin (use * for a wildcard)
+        config.addAllowedOrigin("*");
+
+        // Allow common HTTP methods
+        config.addAllowedMethod("GET");
+        config.addAllowedMethod("POST");
+        config.addAllowedMethod("PUT");
+        config.addAllowedMethod("DELETE");
+
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter((CorsConfigurationSource) source);
     }
 }
